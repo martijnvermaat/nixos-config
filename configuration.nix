1e015a20ef39497bc3ef8adc3da959720d776a15
Martijn Vermaat <martijn@vermaat.name>
2016-07-15 10:48:58 +0200

    Aerosmith

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,9 @@
 
   fileSystems."/".options = [ "noatime" "nodiratime" "discard" ];
 
+  # Oh yes living on the edge.
+  boot.kernelPackages = pkgs.linuxPackages_4_6;
+
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
